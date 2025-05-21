56e05fa58d6e3692f46c69f5feb6777638488fbe
Alban Siffer <31479857+asiffer@users.noreply.github.com>
2025-05-21 09:30:38 +0200

    echarts extension based on pymdown block api

--- shadcn/extensions/echarts/alpha.py
+++ shadcn/extensions/echarts/alpha.py
@@ -1,82 +1,137 @@
+import logging
 import random
-import re
 import string
 import xml.etree.ElementTree as etree
+from typing import Literal
 
-from markdown.blockprocessors import BlockProcessor
-from markdown.extensions import Extension
+from markdown import Markdown
+from pymdownx.blocks import BlocksExtension, BlocksProcessor  # type: ignore
+from pymdownx.blocks.block import Block, type_string_in  # type: ignore
+
+log = logging.getLogger(f"mkdocs.extensions.{__name__}")
 
 
 def generate_random_string(size: int) -> str:
     return "".join(random.choices(string.ascii_letters + string.digits, k=size))
 
 
-class EChartsBlockProcessor(BlockProcessor):
-    RE_FENCE_START = re.compile(r"^\s*[+]{3}echarts\s*")
-    RE_FENCE_END = re.compile(r"\s*[+]{3}\s*$")
+class EchartsBlock(Block):
+    NAME = "echarts"
+    ARGUMENT = False
+    OPTIONS = {
+        "renderer": ("svg", type_string_in(["svg", "canvas"])),
+    }
 
-    in_block = False
-    block = ""
+    md: Markdown
 
-    def test(self, parent, block):
-        return self.in_block or re.match(self.RE_FENCE_START, block)
+    # custom state
+    echarts_options: str
+    raw_content: str
+    div_id: str
 
-    def run(self, parent, blocks):
-        self.in_block = True
-        while blocks:
-            self.block += "\n" + blocks.pop(0).strip()
-            if self.RE_FENCE_END.search(self.block):
-                self.in_block = False
+    def on_init(self):
+        self.echarts_options = ""
+        self.div_id = ""
+        self.raw_content = ""
 
-                self.block = self.RE_FENCE_START.sub("", self.block)
-                self.block = self.RE_FENCE_END.sub("", self.block)
-                # keep only the js object
-                self.block = self.block[
-                    self.block.index("{") : (self.block.rindex("}") + 1)
-                ]
+    @property
+    def attrs(self) -> dict:
+        return self.options.get("attrs", {})
 
-                div_id = generate_random_string(6)
-                div = etree.SubElement(parent, "div")
-                div.set("id", div_id)
-                div.set("class", "echarts")
-                div.set("style", "width:100%;height:400px;")
+    @property
+    def renderer(self) -> Literal["svg", "canvas"]:
+        return self.options.get("renderer", "svg")
 
-                script = etree.SubElement(parent, "script")
-                script.set("type", "text/javascript")
-                script.set("defer", "true")
+    def on_create(self, parent: etree.Element):  # type: ignore
+        """Called when a block is initially found and initialized.
+        The on_create method should create the container for the
+        block under the parent element. Other child elements can
+        be created on the root of the container, but outer element
+        of the created container should be returned.
+        """
 
-                script.text = f"""
-                const target{div_id} = document.getElementById('{div_id}');
-                const chart{div_id} = echarts.init(target{div_id}, 'shadcn', {{ renderer: 'canvas' }});
-                chart{div_id}.setOption({self.block});
-                window.addEventListener('resize', function() {{ chart{div_id}.resize(); }});
-                const observer{div_id} = new IntersectionObserver((entries) => {{
-                    entries.forEach(entry => {{
-                        if (entry.isIntersecting) {{
-                            console.log("Target is visible!");
-                            chart{div_id}.resize();
-                            observer.disconnect(); // Stop observing if you want a one-time trigger
-                        }}
-                    }});
-                }});
+        # generate a random id if not provided
+        log.debug("echarts block found")
+        self.div_id = self.attrs.get("id", generate_random_string(6))
+        div = etree.SubElement(parent, "div")
+        return div
 
-                observer{div_id}.observe(target{div_id});
-                """
+    def on_add(self, block: etree.Element):
+        """When any calls occur to process new content, on_add is called.
+        This gives the block a chance to return the element where the
+        content is desired."""
 
-                self.block = ""
+        # save the raw content in all cases
+        self.raw_content = block.text or ""
+        # ignore empty block
+        if block.text is None:
+            return block
+        # check if it looks like a json object
+        start = block.text.find("{")
+        end = block.text.rfind("}")
+        if start == -1 or end == -1:
+            return block
+
+        log.debug(f"echarts options can be extracted from the block")
+        self.echarts_options = block.text[start : end + 1]
+        # remove the text (otherwise the raw text will be displayed)
+        block.text = ""
+        return block
+
+    def on_end(self, block: etree.Element) -> None:
+        """When a block is parsed to completion, the on_end event is
+        executed. This allows an extension to perform any post
+        processing on the elements. You could save the data as raw
+        text and then parse it special at the end or you could walk
+        the HTML elements and move content around, add attributes, or
+        whatever else is needed."""
+        log.debug(f"content of echarts block:\n{self.raw_content}")
+        if self.echarts_options == "":
+            log.warning(
+                f"No echarts options found in the following block:"
+                f"\n---\n{self.raw_content}\n---\n"
+            )
+            return
+
+        script = etree.SubElement(block, "script")
+        script.set("type", "text/javascript")
+        script.set("defer", "true")
+
+        script.text = f"""
+        const target{self.div_id} = document.getElementById('{self.div_id}');
+        const chart{self.div_id} = echarts.init(target{self.div_id}, 'shadcn', {{ renderer: '{self.renderer}' }});
+        chart{self.div_id}.setOption({self.echarts_options});
+        window.addEventListener('resize', function() {{ chart{self.div_id}.resize(); }});
+        const observer{self.div_id} = new IntersectionObserver((entries) => {{
+            entries.forEach(entry => {{
+                if (entry.isIntersecting) {{
+                    chart{self.div_id}.resize();
+                    observer.disconnect(); // Stop observing if you want a one-time trigger
+                }}
+            }});
+        }});
+
+        observer{self.div_id}.observe(target{self.div_id});
+        """
+
+        # set some attributes if not defined by the user
+        block.set("id", self.div_id)
+        block.set("class", block.get("class", "echarts"))
+        block.set("style", block.get("style", "width:100%;height:500px;"))
+
+        self.echarts_options = ""
+        self.div_id = ""
+
+    def on_markdown(self) -> str:  # type: ignore
+        """Check how element should be treated by the Markdown parser."""
+        return "raw"
 
 
-class EChartsExtension(Extension):
-    """Custom extension to register the ECharts BlockProcessor"""
-
-    def extendMarkdown(self, md):
-        md.parser.blockprocessors.register(
-            EChartsBlockProcessor(md.parser),
-            "echarts.beta",
-            20.0,
-        )
+class EchartsBlockExtension(BlocksExtension):
+    def extendMarkdownBlocks(self, md: Markdown, block_mgr: BlocksProcessor):
+        block_mgr.register(EchartsBlock, self.getConfigs())
 
 
-# Register the extension in Python-Markdown
-def makeExtension(**kwargs):
-    return EChartsExtension(**kwargs)
+def makeExtension(*args, **kwargs):
+    """Return extension."""
+    return EchartsBlockExtension(*args, **kwargs)
